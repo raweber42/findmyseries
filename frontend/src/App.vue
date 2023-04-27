abbedccddcd7592d040c5ee85428c96d36970d16
raweber <raweber@student.42wolfsburg.de>
2023-04-27 06:43:53 +0200

    Add conditional rendering for results, bootstrap design improvements

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container">
+  <div class="container content">
     <header>
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <a class="navbar-brand" href="#">findmyseries.com</a>
@@ -32,7 +32,7 @@
     <body>
       <div class="row justify-content-center">
         <div class="col-md-8 col-lg-6">
-          <form>
+          <form @submit.prevent>
             <div class="input-group mb-3">
               <input
               type="text"
@@ -47,31 +47,46 @@
               class="btn btn-outline-secondary btn-sm"
               type="button"
               id="search-btn"
+              @click="runSearch()"
               >
               Search
-            </button>
-          </div>
-        </form>
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
-  </body>
 
-  <div class="container website-body">
-    <div class="container p-3">
-      <div class="row align-items-start">
-        <div class="card-group">
-          <div class="card border-dark text-center shadow-lg ">
-            <img class="card-img-top" src="./assets/download.jpeg" alt="movie picture">
-            <div class="card-body">
-              {{ searchOutput[0] }}
+      <div class="container">
+        <div class="text-center">
+          <div class="spinner-border mt-5" role="status" v-if="searchRunning == true">
+            <span class="visually-hidden">Loading...</span>
+          </div>
+        </div>
+        <div class="container p-3">
+          <div class="row row-cols-1 row-cols-md-3 align-items-start justify-content-center"
+          id="card-row">
+            <div class="col" v-if="searchFinished != false">
+              <div class="card-container">
+                <div class="card border-dark text-center shadow-lgcenter-content">
+                <img class="card-img-top" src="./assets/download.jpeg" alt="movie picture">
+                <div class="card-body">
+                  {{ searchOutput[0] }}
+                </div>
+              </div>
             </div>
           </div>
-          <div class="card border-dark text-center shadow-lg">
+          <div class="col" v-if="searchFinished != false">
+            <div class="card-container">
+              <div class="card border-dark text-center shadow-lg">
                 <img src="./assets/download.jpeg" alt="movie picture">
                 <div class="card-body">
                   {{ searchOutput[1] }}
                 </div>
               </div>
+            </div>
+          </div>
+          <div class="col" v-if="searchFinished != false">
+            <div class="card-container">
               <div class="card border-dark text-center shadow-lg">
                 <img src="./assets/download.jpeg" alt="movie picture">
                 <div class="card-body">
@@ -82,11 +97,17 @@
           </div>
         </div>
       </div>
-      <!-- v-for="output in searchOutput" :key="output MAYBE flex-row!"-->
-      <!-- <footer>
-        Made with ❤️ by <a href="https://www.linkedin.com/in/ralfdimitrijweber/">Ralf</a>
-      </footer> -->
     </div>
+  </body>
+      <!-- v-for="output in searchOutput" :key="output MAYBE flex-row!"-->
+    <footer class="fixed-bottom bg-dark text-light">
+      <div class="container">
+        <p class="text-center mb-0">
+          Made with ❤️ by <a href="https://www.linkedin.com/in/ralfdimitrijweber/">Ralf</a>
+        </p>
+      </div>
+    </footer>
+  </div>
 </template>
 
 <script lang="ts">
@@ -99,19 +120,45 @@ export default defineComponent({
     const store = useUserDataStore();
     const searchInput = ref('');
     const searchOutput = ref([]);
+    const searchFinished = ref(false);
+    const searchRunning = ref(false);
 
     const runSearch = async () => {
+      searchRunning.value = true;
       await DataService.runSearch(searchInput.value)
         .then((response: any) => {
+          console.log(response.data);
           searchOutput.value = response.data;
           searchInput.value = '';
         })
         .catch((e: Error) => {
           console.log(e);
         });
+      searchFinished.value = true;
+      searchRunning.value = false;
     };
+
+    // SAME HEIGHT CARDS
+    function setEqualHeight() {
+      const cardContainers = document.querySelectorAll('#card-row .card-container');
+      let maxHeight = 0;
+      cardContainers.forEach((cardContainer: Element) => {
+        const { height } = cardContainer.getBoundingClientRect();
+        if (height > maxHeight) {
+          maxHeight = height;
+        }
+      });
+      cardContainers.forEach((cardContainer: Element) => {
+        (cardContainer as HTMLElement).style.height = `${maxHeight}px`; // eslint-disable-line no-param-reassign
+      });
+    }
+
+    window.addEventListener('load', setEqualHeight);
+    window.addEventListener('resize', setEqualHeight);
+    // SAME HEIGHT CARDS
+
     return {
-      store, searchInput, searchOutput, runSearch,
+      store, searchInput, searchOutput, searchRunning, searchFinished, runSearch,
     };
   },
 });
@@ -134,6 +181,11 @@ export default defineComponent({
   background-color: grey !important;
   color: white !important; /*WHY NOT WORKING??*/
   transition: .6s;
+}
+
+.card-container {
+  width: 85%;
+  margin: 1rem auto;
 }
 
 /* header {
@@ -184,6 +236,10 @@ footer {
   bottom: 0;
   width: 100%;
 }
+*/
+footer {
+  opacity: 0.97;
+}
 
 footer a:link {
   color: var(--first-highlight-color);
@@ -193,5 +249,10 @@ footer a:link {
 footer a:hover {
   color: var(--first-highlight-color);
   text-decoration: underline;
-} */
+}
+
+footer a:visited {
+  color: var(--first-highlight-color);
+  text-decoration: none;
+}
 </style>
