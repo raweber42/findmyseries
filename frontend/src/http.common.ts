3b0ec488be30fe8a14539f710796c4160a07dbfc
raweber <raweber@student.42wolfsburg.de>
2023-05-25 07:05:59 +0200

    Add cors config for minikube cluster

--- frontend/src/http.common.ts
+++ frontend/src/http.common.ts
@@ -3,8 +3,8 @@ import type { AxiosInstance } from 'axios';
 
 const apiClient: AxiosInstance = axios.create({
   // baseURL: process.env.VITE_BASE_URL,
-  // baseURL: 'http://backend.default.svc.cluster.local:3000', // "default" refers to namespace
-  baseURL: 'http://192.168.59.100/backend:3000', // "default" refers to namespace
+  baseURL: 'http://backend.default.svc.cluster.local:3000', // "default" refers to namespace
+  // baseURL: 'http://192.168.59.100/backend:3000', // "default" refers to namespace
 
   headers: {
     'Content-type': 'application/json',
--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -4,7 +4,7 @@ module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
     allowedHosts: [
-      '0.0.0.0',
+      'all', // https://webpack.js.org/configuration/dev-server/#devserverallowedhosts
     ],
   },
 });
